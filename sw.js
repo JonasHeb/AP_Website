03d04eef6fd709f05e1d02a835856a7578859a97
jonheb <74306537+JonasHeb@users.noreply.github.com>
2025-05-20 10:28:04 +0200

    window sizing

--- sw.js
+++ sw.js
@@ -24,3 +24,12 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+document.addEventListener("DOMContentLoaded", event => {
+   // we can move only if we are not in a browser's tab
+   isBrowser = matchMedia("(display-mode: browser)").matches;
+   if (!isBrowser) {
+      window.moveTo(16, 16);
+      window.resizeTo(800, 600);
+   }
+});
